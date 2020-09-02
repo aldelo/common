753f7b7946699f9d1ec6dc5ed385d67bf4d74b42
feiliu <liu.fei@aldelo.com>
2020-09-02 14:34:54 +0800

    add struct helper function fill can auto copy values from one struct to another struct with same key name

---
+++ helper-struct.go
@@ -0,0 +1,48 @@
+package helper
+
+import (
+	"errors"
+	"reflect"
+)
+
+/*
+ * Copyright 2020 Aldelo, LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// src and dst both must be struct，and dst must be point
+// it will copy the src struct with same tag name as dst struct tag
+func Fill(src interface{}, dst interface{}) error {
+	srcType := reflect.TypeOf(src)
+	srcValue := reflect.ValueOf(src)
+	dstValue := reflect.ValueOf(dst)
+
+	if srcType.Kind() != reflect.Struct {
+		return errors.New("src must be struct")
+	}
+	if dstValue.Kind() != reflect.Ptr {
+		return errors.New("dst must be point")
+	}
+
+	for i := 0; i < srcType.NumField(); i++ {
+		dstField := dstValue.Elem().FieldByName(srcType.Field(i).Name)
+		if dstField.CanSet() {
+			dstField.Set(srcValue.Field(i))
+		}
+	}
+
+	return nil
+}
+
+
